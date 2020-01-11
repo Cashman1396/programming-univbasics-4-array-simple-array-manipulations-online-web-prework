cf708b6c57a7270c459c5cc505e7e01bff1fa8b8
Chris Ashman <chrisashman96@gmail.com>
2020-01-11 21:05:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,4 +23,17 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
-  array.
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq() 
+end
+
+def using_flatten(array)
+  array.flatten()
+end
